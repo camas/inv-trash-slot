2e7351abd7b90c107ae57915564ef0070aabbfab
camas <camas@users.noreply.github.com>
2020-08-07 17:26:37 +0100

    Make onKeyPressed inject less invasive

---
+++ src/main/java/io/github/camas/invtrashslot/mixin/HandledScreenMixin.java
@@ -0,0 +1,71 @@
+package io.github.camas.invtrashslot.mixin;
+
+import io.github.camas.invtrashslot.InvTrashSlot;
+import io.github.camas.invtrashslot.InvTrashSlotClient;
+import io.netty.buffer.Unpooled;
+import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(HandledScreen.class)
+public abstract class HandledScreenMixin extends Screen {
+
+    protected HandledScreenMixin(Text title) {
+        super(title);
+    }
+
+    @Final
+    @Shadow
+    protected ScreenHandler handler;
+
+    @Shadow
+    protected abstract boolean isPointWithinBounds(int xPosition, int yPosition, int width, int height, double pointX, double pointY);
+
+    /**
+     * Trashes item when delete button pressed
+     */
+    @Inject(method = "keyPressed", at = @At("HEAD"), cancellable = true)
+    private void InvTrashSlot$keyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> callback) {
+        // Ignore if not InventoryScreen
+        if (!((Object) this instanceof InventoryScreen)) {
+            return;
+        }
+
+        // Ignore if Delete key not pressed
+        if (!InvTrashSlotClient.deleteKeybind.matchesKey(keyCode, scanCode)) {
+            return;
+        }
+
+        // Copied from GameRenderer
+        int mouseX = (int) (this.client.mouse.getX() * (double) this.client.getWindow().getScaledWidth() / (double) this.client.getWindow().getWidth());
+        int mouseY = (int) (this.client.mouse.getY() * (double) this.client.getWindow().getScaledHeight() / (double) this.client.getWindow().getHeight());
+
+
+        for (Slot slot : this.handler.slots) {
+            if (isPointWithinBounds(slot.x, slot.y, 16, 16, mouseX, mouseY)) {
+                if (slot.getStack().isEmpty()) {
+                    break;
+                }
+                PacketByteBuf packetData = new PacketByteBuf(Unpooled.buffer());
+                packetData.writeInt(slot.id);
+                ClientSidePacketRegistry.INSTANCE.sendToServer(InvTrashSlot.TRASH_SLOT_PACKET_ID, packetData);
+                slot.setStack(ItemStack.EMPTY);
+                break;
+            }
+
+        }
+        callback.setReturnValue(true);
+    }
+}
--- src/main/java/io/github/camas/invtrashslot/mixin/InventoryScreenMixin.java
+++ src/main/java/io/github/camas/invtrashslot/mixin/InventoryScreenMixin.java
@@ -1,19 +1,13 @@
 package io.github.camas.invtrashslot.mixin;
 
 import io.github.camas.invtrashslot.InvTrashSlot;
-import io.github.camas.invtrashslot.InvTrashSlotClient;
-import io.netty.buffer.Unpooled;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
 import net.minecraft.client.gui.screen.ingame.AbstractInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.item.ItemStack;
-import net.minecraft.network.PacketByteBuf;
 import net.minecraft.screen.PlayerScreenHandler;
-import net.minecraft.screen.slot.Slot;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
@@ -50,31 +44,5 @@ public abstract class InventoryScreenMixin extends AbstractInventoryScreen<Playe
     private void InvTrashSlot$drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY, CallbackInfo callback) {
         this.client.getTextureManager().bindTexture(InvTrashSlot$BACKGROUND_TEXTURE);
         drawTexture(matrices, this.x + InvTrashSlot.SLOT_X - 5, this.y + InvTrashSlot.SLOT_Y - 2, 0, 0, 26, 23, 26, 23);
-    }
-
-    /**
-     * Deletes item when delete button pressed
-     */
-    @Override
-    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
-        // From GameRenderer
-        int mouseX = (int) (this.client.mouse.getX() * (double) this.client.getWindow().getScaledWidth() / (double) this.client.getWindow().getWidth());
-        int mouseY = (int) (this.client.mouse.getY() * (double) this.client.getWindow().getScaledHeight() / (double) this.client.getWindow().getHeight());
-        if (InvTrashSlotClient.deleteKeybind.matchesKey(keyCode, scanCode)) {
-            for (Slot slot : this.handler.slots) {
-                if (isPointWithinBounds(slot.x, slot.y, 16, 16, mouseX, mouseY)) {
-                    if (slot.getStack().isEmpty()) {
-                        break;
-                    }
-                    PacketByteBuf packetData = new PacketByteBuf(Unpooled.buffer());
-                    packetData.writeInt(slot.id);
-                    ClientSidePacketRegistry.INSTANCE.sendToServer(InvTrashSlot.TRASH_SLOT_PACKET_ID, packetData);
-                    slot.setStack(ItemStack.EMPTY);
-                    break;
-                }
-            }
-        }
-
-        return super.keyPressed(keyCode, scanCode, modifiers);
     }
 }
